ae46657f395d2cc8eba463ef035c28c9ae90b45f
Vinicius Rocha <vmrocha@gmail.com>
2021-06-28 22:10:07 -0400

    First solution using .index method

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,7 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
-
+module ResistorColorDuo
+  COLORS = [ "black", "brown", "red", "orange", "yellow",
+              "green", "blue", "violet", "grey", "white"]
+  def self.value(input)
+    COLORS.index(input[0]) * 10 + COLORS.index(input[1])
+  end
+end

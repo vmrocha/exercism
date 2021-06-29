2018319cdddd905e30ffcf80507ba056a7cec7fe
Vinicius Rocha <vmrocha@gmail.com>
2021-06-29 07:41:15 -0400

    Implement IsAllergicTo method

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -1,26 +1,30 @@
 using System;
 
+[Flags]
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
 }
 
 public class Allergies
 {
+    private readonly Allergen _allergies;
+
     public Allergies(int mask)
     {
+        _allergies = (Allergen)mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _allergies.HasFlag(allergen);
     }
 
     public Allergen[] List()

9b030930e21488a16b08e880dcdc69d9db0edc94
Vinicius Rocha <vmrocha@gmail.com>
2021-06-30 20:29:32 -0400

    Update resistor color duo branch

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,8 +1,17 @@
-module ResistorColor
-    COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"].freeze
-
-    def self.color_code(color)
-        COLORS.find_index(color)
-    end
+module ResistorColorDuo
+  BANDS = {
+    "black"  => 0,
+    "brown"  => 1,
+    "red"    => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green"  => 5,
+    "blue"   => 6,
+    "violet" => 7,
+    "grey"   => 8,
+    "white"  => 9
+  }
+  def self.value(bands)
+    bands.take(2).map { |b| BANDS[b] }.join.to_i
+  end
 end
-

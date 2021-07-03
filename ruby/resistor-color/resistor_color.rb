e31397738599f1aaf2d7b612194085009c634764
Vinicius Rocha <vmrocha@gmail.com>
2021-07-02 22:35:49 -0400

    Fix spaces

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,7 +1,18 @@
 module ResistorColor
-  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"].freeze
+  COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+  ].freeze
 
   def self.color_code(color)
-      COLORS.find_index(color)
+    COLORS.find_index(color)
   end
 end

4a52293b65e4fb865614404a7c42bb1f69f3b1fe
Vinicius Rocha <vmrocha@gmail.com>
2021-07-04 21:36:04 -0400

    Implement solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,8 +1,6 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
+module Year
+  def self.leap?(year)
+    year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+  end
+end
 

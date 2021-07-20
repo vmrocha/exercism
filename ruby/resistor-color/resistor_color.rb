ea5ff0fd72ee7773047b80c6d54df6c7958f197f
Vinicius Rocha <vmrocha@gmail.com>
2021-07-19 20:06:12 -0400

    Update constant to use a hash instead

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,18 +1,22 @@
 module ResistorColor
-  COLORS = [
-    "black",
-    "brown",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "violet",
-    "grey",
-    "white"
-  ].freeze
+
+  BAND = {
+    "black"  => 0,
+    "brown"  => 1,
+    "red"    => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green"  => 5,
+    "blue"   => 6,
+    "violet" => 7,
+    "grey"   => 8,
+    "white"  => 9
+  }
+
+  COLORS = BAND.keys
 
   def self.color_code(color)
-    COLORS.find_index(color)
+    BAND[color]
   end
+
 end

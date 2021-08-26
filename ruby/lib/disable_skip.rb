2ebe1970e29c2efcc2208567f8c4c2999c6d9ef2
Vinicius Rocha <vmrocha@gmail.com>
2021-08-26 17:28:15 -0400

    Add helper lib to disable all skips

---
+++ ruby/lib/disable_skip.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+# You can use this to disabe all skips in the current exercise by issuing the
+# following command:
+#     ruby -I../lib -rdisable_skip <filename_test.rb>
+
+module Minitest
+  class Test
+    def skip(_msg='', _bt=caller)
+    end
+  end
+end
+
